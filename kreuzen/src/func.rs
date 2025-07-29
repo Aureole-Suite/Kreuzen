bc5f7de931b25cea3966dd375dcb8e078cb2ba78
Kyuuhachi <caagr98@gmail.com>
2025-07-29 14:56:37 +0200

    Invert ~ in printing

--- kreuzen/src/func.rs
+++ kreuzen/src/func.rs
@@ -30,7 +30,7 @@ impl OpMeta {
 impl std::fmt::Debug for OpMeta {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		write!(f, "{}", self.line)?;
-		if self.width != 0xFF {
+		if self.width == 0xFF {
 			write!(f, "~")?;
 		}
 		Ok(())
